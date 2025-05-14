281bab87194f332c5d949ab45c0042e1260d7f6f
S4ntos25 <132910423+S4ntos25@users.noreply.github.com>
2025-05-14 16:21:23 -0300

    Update and rename index.js to marcas.js

---
+++ marcas.js
@@ -0,0 +1,11 @@
+const carro = new Carro('Fiat', 'Uno', 2015, 4);
+carro.ligar();
+carro.abrirPorta();
+
+const moto = new Moto('Honda', 'CG', 2010, 150);
+moto.ligar();
+moto.empinar();
+
+const carro2 = new Carro('Volkswagen', 'Gol', 2018, 4);
+carro2.ligar();
+carro2.desligar();
