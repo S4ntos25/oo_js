4aa330348397011e4bb8c8318fd36418de77d123
S4ntos25 <132910423+S4ntos25@users.noreply.github.com>
2025-05-14 17:15:10 -0300

    Update veiculos.js

--- veiculos.js
+++ veiculos.js
@@ -36,3 +36,6 @@ class Carro extends Veiculo {
   }
 }
 
+var moto = new Moto('Honda', 'CG', '2021', '160');
+var carro = new Carro('Fiat', 'Uno', '2015', '1000', 4);
+var carro2 = new Carro('Volkswagen', 'Gol', '2018', '1000', 4);
