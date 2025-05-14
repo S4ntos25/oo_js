f1c00170356357307708c161e751031b03801146
S4ntos25 <132910423+S4ntos25@users.noreply.github.com>
2025-05-14 17:05:21 -0300

    Update marcas.js

--- marcas.js
+++ marcas.js
@@ -1,11 +1,17 @@
-const carro = new Carro('Fiat', 'Uno', 2015, 4);
-carro.ligar();
-carro.abrirPorta();
+// Instâncias
+var moto = new Moto('Honda', 'CG', '2021', '160');
+var carro = new Carro('Fiat', 'Uno', '2015', '1000', 4);
+var carro2 = new Carro('Volkswagen', 'Gol', '2018', '1000', 4);
 
-const moto = new Moto('Honda', 'CG', 2010, 150);
+// Testando as instâncias
+console.log(moto);
 moto.ligar();
 moto.empinar();
 
-const carro2 = new Carro('Volkswagen', 'Gol', 2018, 4);
+console.log(carro);
+carro.ligar();
+carro.abrirPorta();
+
+console.log(carro2);
 carro2.ligar();
 carro2.desligar();
