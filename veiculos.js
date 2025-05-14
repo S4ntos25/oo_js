e178d5c3b587772e6b20a7f77dc2a84dad2ccc9d
S4ntos25 <132910423+S4ntos25@users.noreply.github.com>
2025-05-14 16:21:47 -0300

    Create veiculos.js

---
+++ veiculos.js
@@ -0,0 +1,21 @@
+class Carro extends Veiculo {
+  constructor(marca, modelo, ano, portas) {
+    super(marca, modelo, ano);
+    this.portas = portas;
+  }
+
+  abrirPorta() {
+    console.log(`A porta do ${this.modelo} foi aberta.`);
+  }
+}
+
+class Moto extends Veiculo {
+  constructor(marca, modelo, ano, cilindradas) {
+    super(marca, modelo, ano);
+    this.cilindradas = cilindradas;
+  }
+
+  empinar() {
+    console.log(`A ${this.modelo} está empinando.`);
+  }
+}
